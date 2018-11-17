d82a28296b33011d569efc3eea24f92d2143fb87
tsu-nera <fox10225fox@gmail.com>
2018-11-17 17:50:07 +0900

    initial commit

---
+++ spec/mini/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Mini::Rails do
+  it "has a version number" do
+    expect(Mini::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
