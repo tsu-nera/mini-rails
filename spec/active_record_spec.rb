c507275dbfee1aac59dc2f6806f4c1e45c4a3810
tsu-nera <fox10225fox@gmail.com>
2018-11-17 19:16:28 +0900

    implement activerecord find

--- spec/active_record_spec.rb
+++ spec/active_record_spec.rb
@@ -5,10 +5,38 @@ require 'muffin_blog/app/models/application_record'
 require 'muffin_blog/app/models/post'
 
 RSpec.describe 'ActiveRecord' do
-  let(:post) { Post.new(id: 1, title: 'My first post') }
-
-  it '#initialize' do
-    expect(post.id).to eq(1)
-    expect(post.title).to eq('My first post')
+  before do
+    Post.establish_connection(
+        database: "#{__dir__}/muffin_blog/db/development.sqlite3")
   end
+
+  describe "#initialize" do
+    let(:post) { Post.new(id: 1, title: 'My first post') }
+
+    it 'pass' do
+      expect(post.id).to eq(1)
+      expect(post.title).to eq('My first post')
+    end
+  end
+
+  describe '#find' do
+    let(:post) { Post.find(1) }
+
+    it 'pass' do
+      expect(post).kind_of? Post
+      expect(post.id).to eq(1)
+      expect(post.title).to eq('Blueberry Muffins')
+    end
+  end
+
+  describe "#execute_sql" do
+    it 'pass' do
+      rows = Post.connection.execute('SELECT * FROM posts')
+      expect(rows).kind_of? Array
+      row = rows.first
+      expect(row).kind_of? Hash
+      expect(row.keys).to eq([:id, :title, :body, :created_at, :updated_at])
+    end
+  end
+
 end
